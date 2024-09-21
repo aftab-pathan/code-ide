d6ee321d05bfaad885ac1f1bc1a6e790fbdd43a9
Aftab Pathan <91469681+aftab-pathan@users.noreply.github.com>
2024-09-21 14:08:52 +0000

    Implemented Client Code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import NoPage from "./pages/NoPage";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Editior from "./pages/Editior";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  let isLoggedIn = localStorage.getItem("isCodeIdeLoggedIn");
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={isLoggedIn ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/editior/:projectID"
+            element={isLoggedIn ? <Editior /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="*"
+            element={isLoggedIn ? <NoPage /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
